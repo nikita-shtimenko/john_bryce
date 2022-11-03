c0c55bbad21ae991e8d87a34bdd901a67b078ba3
Nikita Shtimenko <nikitashtimenko.dev@gmail.com>
2022-11-03 20:46:02 +0200

    Loops: case_16 added.

---
+++ intro_to_programming_28036/loops/4_2/case_16.py
@@ -0,0 +1,10 @@
+def main():
+    first_number = int(input("Enter first number: "))
+    second_number = int(input("Enter second number: "))
+
+    print(pow(first_number, second_number))
+    return 1
+
+
+if __name__ == '__main__':
+    main()
