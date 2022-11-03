b12046970ac3a75183ae2c774c81256835855fe6
Nikita Shtimenko <nikitashtimenko.dev@gmail.com>
2022-11-03 17:16:43 +0200

    Added chapter 4 / case_1

---
+++ intro_to_programming_28036/loops/4_2/case_1.py
@@ -0,0 +1,13 @@
+def main():
+    number = int(input("Enter a number: "))
+    temp = 1
+
+    while temp <= number:
+        print(temp)
+        temp += 1
+
+    return 1
+
+
+if __name__ == '__main__':
+    main()
